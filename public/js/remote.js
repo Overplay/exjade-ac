3ceb89931e6f1129ac615ac757b9a8019ec71853
Mitch Kahn <mitch@ourglass.io>
2017-10-10 16:35:23 -0700

    Play audio mini-app.

--- public/js/remote.js
+++ public/js/remote.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
         //$(".ogaudio").data("protocol", audioId);
         $(".ogaudio").get().forEach(function(element, index, array) {
                 console.log(array[index]);
-            array[index].dataset['url'] = "ws://192.168.1.172:4000/as/";
+            array[index].dataset['url'] = "ws://192.241.217.88:4000/as/";
             array[index].dataset['protocol'] = audioId;
             }
         );
